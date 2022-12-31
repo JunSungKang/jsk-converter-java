4d95b19af1362a2a756bb223a5b61bd8f108439a
JunSungKang <kjuns1@naver.com>
2022-12-31 11:15:48 +0900

    :triangular_flag_on_post: feat(XmlConvert.java): Support xml attribute.
    - Test code update.

--- src/main/java/kr/geneus/jskang/converter/xml/XmlConvert.java
+++ src/main/java/kr/geneus/jskang/converter/xml/XmlConvert.java
@@ -6,6 +6,8 @@ import java.io.RandomAccessFile;
 import java.io.StringReader;
 import java.util.EmptyStackException;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import javax.naming.SizeLimitExceededException;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,6 +16,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import kr.geneus.jskang.converter.common.Convert;
 import lombok.extern.slf4j.Slf4j;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -37,7 +40,7 @@ public class XmlConvert implements Convert {
 			throw new SizeLimitExceededException("Files larger than 2 GB cannot be parsed.");
 		}
 
-		byte[] xml = new byte[(int)fileSize];
+		byte[] xml = new byte[(int) fileSize];
 		randomAccessFile.read(xml);
 		randomAccessFile.close();
 
@@ -70,40 +73,84 @@ public class XmlConvert implements Convert {
 		NodeList nList = doc.getElementsByTagName(rootName);
 
 		Map<String, Object> map = new LinkedHashMap<>();
+		map.put("attributes", new LinkedHashMap<>());
+		map.put("value", new LinkedHashMap<>());
+
 		map.put(rootName, null);
 		log.debug("XML RootName: " + rootName);
-
-		return travNode(map.get(rootName), nList);
+		return travNode(
+			((Map) map.get("attributes")).get(rootName),
+			((Map) map.get("value")).get(rootName),
+			nList
+		);
 	}
 
-	private Map travNode(Object map, NodeList nodes) {
+	private Map travNode(Object attributes, Object value, NodeList nodes) {
 
 		for (int i = 0; i < nodes.getLength(); i++) {
 			Node n = nodes.item(i);
 
+			// Generate attribute start.
+			NamedNodeMap attributeNodes = n.getAttributes();
+			int attributeCnt = attributeNodes.getLength();
+			if (attributes == null) {
+				attributes = new LinkedHashMap<>();
+			}
+
+			if (attributeCnt >= 1) {
+				for (int idx = 0; idx < attributeCnt; idx++) {
+					Node attributeNode = attributeNodes.item(idx);
+					if (((Map) attributes).get(n.getNodeName()) == null) {
+						List<String> attribute = new LinkedList<>();
+						attribute.add(attributeNode.getNodeValue());
+
+						Map<String, List> tAttributes = new LinkedHashMap<>();
+						tAttributes.put(attributeNode.getNodeName(), attribute);
+						((Map) attributes).put(n.getNodeName(), tAttributes);
+					} else {
+						Map a = (Map) ((Map) attributes).get(n.getNodeName());
+						if (a.get(attributeNode.getNodeName()) == null) {
+							List<String> attribute = new LinkedList<>();
+							attribute.add(attributeNode.getNodeValue());
+							a.put(attributeNode.getNodeName(), attribute);
+						} else {
+							((List) a.get(attributeNode.getNodeName())).add(attributeNode.getNodeValue());
+						}
+					}
+				}
+			} else {
+				((Map) attributes).put(n.getNodeName(), new LinkedHashMap<>());
+			}
+			// Generate attribute end.
+
 			if (n.getNodeType() == Node.ELEMENT_NODE) {
+				// Generate child node start.
 				NodeList childNodes = n.getChildNodes();
-				int cnt = childNodes.getLength();
-
-				if (cnt == 1) {
-					if (map == null) {
-						map = new LinkedHashMap<>();
-						log.debug("XML create linkedHashMap.");
-					}
-					((Map) map).put(n.getNodeName(), n.getTextContent());
-				} else {
-					if (map == null) {
-						map = new LinkedHashMap<>();
-						log.debug("XML create linkedHashMap.");
-					}
-					((Map) map).put(n.getNodeName(), null);
+				int nodeCnt = childNodes.getLength();
+				if (value == null) {
+					value = new LinkedHashMap<>();
+					log.debug("XML create linkedHashMap.");
 				}
 
-				if (cnt > 1) {
-					((Map) map).put(n.getNodeName(), travNode(((Map) map).get(n.getNodeName()), childNodes));
+				if (nodeCnt <= 1) {
+					((Map) value).put(n.getNodeName(), n.getTextContent());
 				}
+				if (nodeCnt > 1) {
+					if (((Map) value).get(n.getNodeName()) instanceof String) {
+						((Map) value).put(n.getNodeName(), new LinkedHashMap<>());
+					}
+					Map<String, Object> node = travNode(((Map) attributes).get(n.getNodeName()), ((Map) value).get(n.getNodeName()), childNodes);
+
+					Map tValue = (Map) node.get("value");
+					((Map) value).put(n.getNodeName(), tValue);
+				}
+				// Generate child node end.
 			}
 		}
-		return (Map) map;
+
+		Map<String, Object> data = new LinkedHashMap<>();
+		data.put("attributes", attributes);
+		data.put("value", value);
+		return (Map) data;
 	}
 }
