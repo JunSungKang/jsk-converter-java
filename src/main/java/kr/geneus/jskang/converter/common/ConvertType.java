6792facc1944501535a50835c5a3811453f5f062
JunSungKang <kjuns1@naver.com>
2023-01-08 14:35:28 +0900

    :recycle: refactor(ConvertType.java): convert type class file add.

---
+++ src/main/java/kr/geneus/jskang/converter/common/ConvertType.java
@@ -0,0 +1,8 @@
+package kr.geneus.jskang.converter.common;
+
+public class ConvertType {
+	public static final String XML = "XML";
+	public static final String JSON = "JSON";
+	public static final String MAP = "MAP";
+	public static final String CSV = "CSV";
+}
